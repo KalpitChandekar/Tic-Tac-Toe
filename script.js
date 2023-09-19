c4d113051fa88e54b181a86c2263e2b5ebf82654
Kalpit Chandekar <kalpitchandekar1736@gmail.com>
2023-09-19 21:15:01 +0530

    add game logic

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
 let music = new Audio("music.mp3");
-let turn = new Audio("ting.mp3");
+let audioTurn = new Audio("ting.mp3");
 let gameover = new Audio("gameover.mp3");
-let turn = "x";
+let turn = "X"; // Change "0" to "X" for the initial turn
 
-
-const changeturn = () => {
-  return turn === "x" ? "0" : "X";
+// Function to change the turn
+const changeTurn = () => {
+  turn = turn === "X" ? "O" : "X"; // Update the turn variable
+  return turn;
 };
+
+// Function to check for a win
+const checkWin = () => {
+  // Implement the win-checking logic here
+};
+
+// Game Logic
+let boxes = document.getElementsByClassName("box");
+
+Array.from(boxes).forEach((element) => {
+  let boxtext = element.querySelector(".boxtext");
+  element.addEventListener("click", () => {
+    if (boxtext.innerText === "") {
+      boxtext.innerText = turn;
+      changeTurn();
+      audioTurn.play();
+      checkWin(); // Call the checkWin function after each move
+      document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+    }
+  });
+});
