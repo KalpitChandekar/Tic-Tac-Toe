7359841b6674c19365b57547e0ad6c9fee45d76e
Kalpit Chandekar <kalpitchandekar1736@gmail.com>
2023-09-20 20:23:09 +0530

    created function to check for a win

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let music = new Audio("music.mp3");
 let audioTurn = new Audio("ting.mp3");
 let gameover = new Audio("gameover.mp3");
-let turn = "X"; // Change "0" to "X" for the initial turn
+let turn = "X";
+let isgameover = false; // Change "0" to "X" for the initial turn
 
 // Function to change the turn
 const changeTurn = () => {
@@ -11,7 +12,28 @@ const changeTurn = () => {
 
 // Function to check for a win
 const checkWin = () => {
-  // Implement the win-checking logic here
+  let boxtext = document.getElementsByClassName("boxtext");
+  let win = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  win.forEach((e) => {
+    if (
+      boxtext[e[0]].innerText === boxtext[e[1]].innerText &&
+      boxtext[e[2]].innerText === boxtext[e[1]].innerText &&
+      boxtext[e[0]].innerText !== ""
+    ) {
+      document.querySelector(".info").innerText =
+        boxtext[e[0]].innerText + " won";
+        isgameover = true;
+    }
+  });
 };
 
 // Game Logic
@@ -22,10 +44,13 @@ Array.from(boxes).forEach((element) => {
   element.addEventListener("click", () => {
     if (boxtext.innerText === "") {
       boxtext.innerText = turn;
-      changeTurn();
+      turn = changeTurn();
       audioTurn.play();
       checkWin(); // Call the checkWin function after each move
-      document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+      if (!isgameover) {
+        document.getElementsByClassName("info")[0].innerText =
+          "Turn for " + turn;
+      }
     }
   });
 });
