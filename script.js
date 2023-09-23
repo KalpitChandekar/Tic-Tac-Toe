c962ec442736dc1294b17b048fdacae2df84b60d
Kalpit Chandekar <kalpitchandekar1736@gmail.com>
2023-09-23 20:56:06 +0530

    passes the line through the winning boxes

--- script.js
+++ script.js
@@ -15,14 +15,14 @@ const changeTurn = () => {
 const checkWin = () => {
   let boxtext = document.getElementsByClassName("boxtext");
   let win = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
+    [0, 1, 2, 5, 5, 0],
+    [3, 4, 5, 5, 15, 0],
+    [6, 7, 8, 5, 25, 0],
+    [0, 3, 6, -5, 15, 90],
+    [1, 4, 7, 5, 15, 90],
+    [2, 5, 8, 15, 15, 90],
+    [0, 4, 8, 5, 15, 45],
+    [2, 4, 6, 5, 15, 135],
   ];
   win.forEach((e) => {
     if (
@@ -34,6 +34,8 @@ const checkWin = () => {
         boxtext[e[0]].innerText + " won";
       isgameover = true;
       document.querySelector(".imgbox img ").style.width = "200px";
+      document.querySelector(".line").style.width = "20vw";
+      document.querySelector(".line").style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`;
     }
   });
 };
@@ -76,6 +78,6 @@ resetButton.addEventListener("click", () => {
 
   // You can also reset the image width here if needed
   document.querySelector(".imgbox img").style.width = "0px";
+
+  document.querySelector(".line").style.width = "0vw";
 });
-
-
